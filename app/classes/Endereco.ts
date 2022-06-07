ec6f6c61338f81d3849789d2ea2e603b14be19c0
kermitcosta <kermitcst@gmail.com>
2022-06-07 08:46:41 -0300

    Classe Endereco adicionada

---
+++ app/classes/Endereco.ts
@@ -0,0 +1,56 @@
+export class Endereco {
+    private _cep: string
+    private _logradouro: string
+    private _numero: string
+    private _complemento: string
+    private _cidade: string
+    private _uf: string
+
+    public get cep(): string {
+        return this._cep
+    }
+
+    public set cep(value: string) {
+        this._cep = value
+    }
+
+    public get logradouro(): string {
+        return this._logradouro
+    }
+
+    public set logradouro(value: string) {
+        this._logradouro = value
+    }
+
+    public get numero(): string {
+        return this._numero
+    }
+
+    public set numero(value: string) {
+        this._numero = value
+    }
+
+    public get complemento(): string {
+        return this._complemento
+    }
+
+    public set complemento(value: string) {
+        this._complemento = value
+    }
+
+    public get cidade(): string {
+        return this._cidade
+    }
+
+    public set cidade(value: string) {
+        this._cidade = value
+    }
+
+    public get uf(): string {
+        return this._uf
+    }
+
+    public set uf(value: string) {
+        this._uf = value
+    }
+}
---
+++ app/principal.ts
@@ -0,0 +1,11 @@
+import { Endereco } from "./classes/Endereco.js"
+
+let end = new Endereco()
+end.cep = "13054-620"
+end.logradouro = "Rua Igaci"
+end.numero = "1472"
+end.complemento = "ap 607"
+end.cidade = "Campinas"
+end.uf = "SP"
+
+console.log(end)
